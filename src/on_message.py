f9bd4c7204adfe001d706bbde31a0889d6472e77
Willy <123847330+willy-270@users.noreply.github.com>
2024-10-16 15:40:12 -0500

    ❤️‍🩹

--- src/on_message.py
+++ src/on_message.py
@@ -69,13 +69,13 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
             if reaction.count != 3:
                 return      
 
-    if not already_reacted:
-        embed = discord.Embed(title=message.author.display_name, 
+    embed = discord.Embed(title=message.author.display_name, 
                               description=f"{message.content}\n\n{message.jump_url}",
                               color=discord.Color.green())
-        embed.set_thumbnail(url=message.author.avatar.url if message.author.avatar else message.author.default_avatar)
-        embed.set_footer(text=f"{message.created_at.replace(tzinfo=timezone('UTC')).astimezone(timezone('America/Chicago')).strftime('%-m/%-d/%Y, %-I:%M %p')}")
-        hof_msg = await bot.get_channel(HALL_OF_FAME_CHANNEL_ID).send(embed=embed)
-
-        await message.reply(f"Added to hall of fame.\n{hof_msg.jump_url}", mention_author=False)
+    embed.set_thumbnail(url=message.author.avatar.url if message.author.avatar else message.author.default_avatar)
+    embed.set_footer(text=f"{message.created_at.replace(tzinfo=timezone('UTC')).astimezone(timezone('America/Chicago')).strftime('%-m/%-d/%Y, %-I:%M %p')}")
+    hof_msg = await bot.get_channel(HALL_OF_FAME_CHANNEL_ID).send(embed=embed)
+    
+    await message.add_reaction("🔥")
+    await message.reply(f"Added to hall of fame.\n{hof_msg.jump_url}", mention_author=False)
 
